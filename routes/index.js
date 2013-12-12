13e7938462133d178a38b9ffe746d81ef161a53b
XiNGRZ <chenxingyu92@gmail.com>
2013-12-12 13:09:39 +0800

    calendar ui very good

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 // deps
 var passport = require('passport')
+  , sise = require('../lib/sise')
+  , schedule = require('../lib/sise/parsers/schedule')
 
 exports.signin = function (req, res, error) {
   return res.render('signin')
@@ -16,6 +18,16 @@ exports.signout = function (req, res, error) {
 }
 
 exports.show = function (req, res, error) {
-  console.log(req.session)
-  return res.render('index')
+  sise.calendar(req.session.jar, 2011, 2, function (err, calendar) {
+    if (err) {
+      return error(err)
+    }
+
+    if (!calendar) {
+      req.logout()
+      return res.redirect('/')
+    }
+
+    return res.render('index', { schedule: schedule.table(calendar) })
+  })
 }
