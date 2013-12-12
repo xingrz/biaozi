85004330c43d1a031ed92c00239f771dd0845776
XiNGRZ <chenxingyu92@gmail.com>
2013-12-12 17:40:32 +0800

    ajax ok

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -15,11 +15,61 @@ $(function () {
     schedule.forEach(function (row, time) {
       row.forEach(function (cell, day) {
         cell.forEach(function (klass) {
-          var html = ''
+          var week = klass.week
+            , html = ''
 
-          html += '<li style="background:#' + color(klass.name) + '">'
+          html += '<li style="background:' + color(klass.name) + '">'
+          html += '<h3>' + klass.name + '</h3>'
+          html += '<p class="details">'
+          html += '<span class="class">' + klass.code + '</span>'
+          html += '<span class="teacher">' + klass.teacher + '</span>'
 
+          if (week.which) {
+            html += '<span class="weeks">第 ' + week.which + ' 周</span>'
+          } else if (week.weeks) {
+            html += '<span class="weeks">第 ' + week.weeks.join(' ') + ' 周</span>'
+          } else if (week.interval) {
+            switch (week.interval) {
+              case 1:
+                html += '<span class="weeks">第 ' + week.start + ' - ' + week.end + ' 周</span>'
+                break
+              case 2:
+                if (week.start % 2) {
+                  html += '<span class="weeks">第 ' + week.start + ' - ' + week.end + ' 周单周</span>'
+                }
+                else {
+                  html += '<span class="weeks">第 ' + week.start + ' - ' + week.end + ' 周双周</span>'
+                }
+                break
+              default:
+                html += '<span class="weeks">第 ' + week.start + ' - ' + week.end + ' 周每 ' + week.interval + ' 周</span>'
+                break
+            }
+          }
 
+          html += '<span class="teacher">' + klass.location + '</span>'
+          html += '</p>'
+
+          var weeks = []
+          for (var i = 0; i < 20; i++) {
+            weeks[i] = false
+          }
+
+          if (week.which) {
+            weeks[week.which - 1] = true
+          } else if (week.weeks) {
+            week.weeks.forEach(function (w) {
+              weeks[w - 1] = true
+            })
+          } else if (week.interval) {
+            for (var w = week.start; w <= week.end; w += week.interval) {
+              weeks[w - 1] = true
+            }
+          }
+
+          html += '<p class="graph">'
+          html += weeks.map(function (w) { return w ? '<i class="y"></i>' : '<i></i>' }).join('')
+          html += '</p>'
 
           html += '</li>'
 
@@ -30,50 +80,3 @@ $(function () {
 
   }, 'json')
 })
-
-/*
-
-                    li(style='background:#{color(klass.name)}')
-                      week = klass.week
-                      h3 #{klass.name}
-                      p.details
-                        span.class #{klass.code}
-                        span.teacher #{klass.teacher}
-                        span.weeks
-                          if week.which
-                            | 第 #{week.which} 周
-                          else if week.weeks
-                            | 第 #{week.weeks.join(' ')} 周
-                          else if week.interval
-                            case week.interval
-                              when 1
-                                | 第 #{week.start} - #{week.end} 周
-                              when 2
-                                if week.start % 2
-                                  | 第 #{week.start} - #{week.end} 周单周
-                                else
-                                  | 第 #{week.start} - #{week.end} 周双周
-                              default
-                                | 第 #{week.start} - #{week.end} 周每隔 #{week.interval} 周
-                        span.location #{klass.location}
-                      p.graph
-                        - var weeks = []
-                        - for (var i = 0; i < 20; i++)
-                          - weeks[i] = 0
-
-                        if week.which
-                          - weeks[week.which - 1] = 1
-                        else if week.weeks
-                          each w in week.weeks
-                            - weeks[w - 1] = 1
-                        else if week.interval
-                          - for (var w = week.start; w <= week.end; w += week.interval)
-                            - weeks[w - 1] = 1
-
-                        each w in weeks
-                          if w == 1
-                            i.y
-                          else
-                            i
-
-*/
